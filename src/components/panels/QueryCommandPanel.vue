539376cb9a3132346dc796d90068251d8e9de677
Vladimir Minkin <vladimir.minkin@gmail.com>
2022-10-30 23:15:40 +0300

    QueryCommandPanel updated with use of flex-row/col

--- src/components/panels/QueryCommandPanel.vue
+++ src/components/panels/QueryCommandPanel.vue
@@ -24,32 +24,29 @@ const onCleanQuery = () => {
 </script>
 
 <template>
-  <div class="flex-col w-full">
-    <div class="flex flex-row">
+  <flex-col class="flex w-full space-y-1">
+    <flex-row>
       <small class="text-gray-500 px-1">Enter SQL command:</small>
-    </div>
-    <div class="flex flex-row py-1">
+    </flex-row>
+    <flex-row>
       <textarea
         class="w-full h-16 text-lg rounded focus:outline-none p-2 border-1 border-gray-500"
         :disabled="isQueryNotSelected"
         :value="query?.command"
         @input="onUpdateSelectedQueryCommand"
       ></textarea>
-    </div>
-    <div class="flex flex-row justify-end items-center mt-1 space-x-4">
-      <div
-        v-if="!isQueryNotSelected"
-        class="flex-col"
-      >
+    </flex-row>
+    <flex-row class="flex justify-end items-center space-x-4">
+      <flex-col v-if="!isQueryNotSelected">
         <button
           :disabled="!canExecuteQuery"
-          class="py-1 px-2 text-gray-400 enabled:hover:text-red-500"
+          class="py-1 px-2 text-gray-400 enabled:hover:text-red"
           @click="onCleanQuery"
         >
           <small>Clean</small>
         </button>
-      </div>
-      <div class="flex-col">
+      </flex-col>
+      <flex-col>
         <button
           :disabled="!canExecuteQuery"
           class="btn btn-rounded-border"
@@ -57,9 +54,9 @@ const onCleanQuery = () => {
         >
           Execute
         </button>
-      </div>
-    </div>
-  </div>
+      </flex-col>
+    </flex-row>
+  </flex-col>
 </template>
 
 <style scoped></style>
